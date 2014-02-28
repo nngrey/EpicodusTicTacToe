c00a4fdbbd838c1b9067f54b44b3d8cc160864cd
Nathan Grey <greynng@gmail.com>
2014-02-27 22:34:12 -0800

    turns working

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var Game = {
+
   create: function() {
     var game = Object.create(Game);
     game.initialize();
@@ -6,26 +7,45 @@ var Game = {
   },
   initialize: function() {
     this.board = Board.create();
+    this.player = Player.create("O");
     this.player = Player.create("X");
-    this.player = Player.create("Y");
- }
+    this.currentPlayer = "Z";
+    console.log(this.currentPlayer);
+    this.turnCounter = 3;
+    this.turn();
+
+ },
   turn: function() {
-    var turnCounter = 0;
-    if (turnCounter % 2 === 0){
-      pl
+ //   var turnCounter = 3;
+//    this.currentPlayer = Player.players[1];
+      this.turnCounter = this.turnCounter + 1;
+    if (this.turnCounter % 2 === 0) {
+      this.currentPlayer = Player.players[0];
+      console.log(this.currentPlayer);
+
+    } else {
+      this.currentPlayer = Player.players[1];
+    //  this.turnCounter += this.turnCounter;
+      console.log(this.currentPlayer);
     }
   }
 }
 
 var Player = {
-  this.players = [];
-  create: function() 
-    return Object.create(Player);
 
+  players: [],
+
+  create: function(symbol) {
+  var player = Object.create(Player);
+  player.initialize(symbol);
+  return player;
   },
+
   initialize: function(symbol) {
     this.symbol = symbol;
-    players.push(this.symbol);
+ //   console.log(this.symbol)
+    this.players.push(this.symbol);
+ //   console.log(Player.players)
   }
 }
 
@@ -46,13 +66,13 @@ var Space = {
 }
  var Board = {
   
-  create: function(){
+  create: function() {
   var board = Object.create(Board);
   board.initialize();
   return board;
-  }
+  },
 
-  initialize: function(){
+  initialize: function() {
     this.spaces = []; 
       for (var j=1; j<=3; j++) {
         for (var k=1; k<=3; k++) {
@@ -64,8 +84,50 @@ var Space = {
     }
  }
 
- 
+$(document).ready(function(){
+  $("#start-game").click(function() {
+    var newGame = Game.create();
+//    console.log(newGame);
 
+ $("#pos1").click(function(){
+      newGame.turn();
+    $("#pos1").text(newGame.currentPlayer);
+ });
+ $("#pos2").click(function(){
+    newGame.turn();
+    $("#pos2").text(newGame.currentPlayer);
+ });
+ $("#pos3").click(function(){
+    newGame.turn();
+    $("#pos3").text(newGame.currentPlayer);
+ });
+ $("#pos4").click(function(){
+    newGame.turn();
+    $("#pos4").text(newGame.currentPlayer);
+ });
+ $("#pos5").click(function(){    
+    newGame.turn();
+    $("#pos5").text(newGame.currentPlayer);
+ });
+ $("#pos6").click(function(){
+    newGame.turn();
+    $("#pos6").text(newGame.currentPlayer);
+ });
+ $("#pos7").click(function(){
+    newGame.turn();
+    $("#pos7").text(newGame.currentPlayer);
+ });
+ $("#pos8").click(function(){
+    newGame.turn();
+    $("#pos8").text(newGame.currentPlayer);
+ });
+ $("#pos9").click(function(){
+    newGame.turn();
+    $("#pos9").text(newGame.currentPlayer);
+ });
+});
+
+});
 
 
 // var winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]; 
@@ -75,42 +137,6 @@ var Space = {
 //     }
 //   });
 // };
-
-$(document).ready(function(){
-  $("#start-game").click(function() {
-    Game.create();
- });
-});
- $("#pos1").click(function(){
-    $("#pos1").text(Player.symbol);
- });
- $("#pos2").click(function(){
-    $("#pos2").text(Player.symbol);
- });
- $("#pos3").click(function(){
-    $("#pos3").text(Player.symbol);
- });
- $("#pos4").click(function(){
-    $("#pos4").text(Player.symbol);
- });
- $("#pos5").click(function(){
-    $("#pos5").text(Player.symbol);
- });
- $("#pos6").click(function(){
-    $("#pos6").text(Player.symbol);
- });
- $("#pos7").click(function(){
-    $("#pos7").text(Player.symbol);
- });
- $("#pos8").click(function(){
-    $("#pos8").text(Player.symbol);
- });
- $("#pos9").click(function(){
-    $("#pos9").text(Player.symbol);
- });
-});
-
-var array = ["#pos1"];
 
 
 // var winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]; 
