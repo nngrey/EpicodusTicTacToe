d48f62707dc55e0344c8b9938b0fbeb61da76224
Nathan Grey & pogawa <student@epicodus.com>
2014-02-27 17:23:44 -0800

    modest progress with Tic Tac Toe

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,60 @@
-describe('functionName', function() {
-  it('does something', function() {
-    functionName("input").should.equal("output");
+describe("Player", function() {
+  describe("initialize", function() {
+    it("is initialized with a symbol", function() {
+      var testPlayer = Object.create(Player);
+      testPlayer.initialize("X");
+      testPlayer.symbol.should.equal("X");
+    });
+  });
+
+  describe("create", function () {
+    it("creates a new Player object", function () {
+      var testPlayer = Player.create("X");
+      Player.isPrototypeOf(testPlayer).should.equal(true);
+    });
   });
 });
 
+describe("Space", function(){
+  describe("initialize", function(){
+    it("is initialized with an x and y coordinate", function() {
+      var testSpace = Object.create(Space);
+      testSpace.initialize(1, 2);
+      testSpace.xCoordinate.should.equal(1);
+      testSpace.yCoordinate.should.equal(2);
+    });
+  });
+
+  describe("create", function() {
+    it("creates a new instance of Space", function() {
+      var testSpace = Space.create();
+      Space.isPrototypeOf(testSpace).should.equal(true);
+    });
+  });
+
+describe("markBy", function(){
+  it("lets the player mark a space", function(){
+    var testPlayer = Player.create("X");
+    var testSpace = Space.create(1,2);
+    testSpace.markBy(testPlayer)
+    testSpace.markedBy.should.equal(testPlayer);
+    });
+  });
+});
+
+describe("Board", function() {
+  it("creates 9 spaces when it is initialized", function() {
+    var testBoard = Object.create(Board);
+    testBoard.initialize(1, 2);
+    testBoard.spaces.length.should.equal(9);
+  });
+});
+
+describe("Game", function () {
+  describe("create", function () {
+    it("creates a new instance of Game", function () {
+      var testGame = Game.create();
+      Game.isPrototypeOf(testGame).should.equal(true);
+     });
+   });
+});
